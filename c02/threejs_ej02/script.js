488890dc90589abe3f1a4d1dd5c69998bc68a161
sergio6000601 <141768666+sergio6000601@users.noreply.github.com>
2023-09-12 15:32:05 -0500

    Add files via upload
    se arreglo camara ortogonal y se añadieron texturas a cada figura
    falta generar la funcion para que alterne entre las camaras

--- c02/threejs_ej02/script.js
+++ c02/threejs_ej02/script.js
@@ -3,8 +3,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 //le añado la camara ortogonal (con un nombre "diferente" para evitar errores)
-const camera1 = new THREE.OrthographicCamera( window.innerWidth / - 1, window.innerWidth / 1, window.innerHeight / 1, window.innerHeight / - 1, 2, 1000 );
-//scene.add( camera1 );
+const camera1 = new THREE.OrthographicCamera(-10, 20, 20, -20, 1, 1000 );
+scene.add( camera1 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -12,32 +12,31 @@ document.body.appendChild( renderer.domElement );
 
 //genero el cubo con boxgeometry
 const geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
-const material1 = new THREE.MeshBasicMaterial( { color: 0x550055 } );
+const material1 = new THREE.MeshNormalMaterial( { color: 0xff00ff } );
 const cube = new THREE.Mesh( geometry, material1 );
 const posicion = cube.position.set(2,0,4,1)
 scene.add( cube );
 
 //genero la esfera con spheregeometry
 const geometry1 = new THREE.SphereGeometry( 1, 32, 32 );
-const material2 = new THREE.MeshBasicMaterial( { color: 0x555500 } );
+const material2 = new THREE.MeshDistanceMaterial( { color: 0xfffff0 } );
 const sphere = new THREE.Mesh( geometry1, material2 );
 const posicion2 = sphere.position.set(8,0,4,1)
-
 scene.add(sphere);
 
 camera.position.z = 10;
-camera.position.x = 5;
+camera1.position.z = 10;
 
-//function animate() {
-	//requestAnimationFrame( animate );
-	//cube.rotation.x += 0.01;
-	//cube.rotation.y += 0.01;
+function animate() {
+	requestAnimationFrame( animate );
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 	
-	//sphere.rotation.x += 0.01;
-	//sphere.rotation.y += 0.01;
+	sphere.rotation.x += 0.01;
+	sphere.rotation.z += 0.01;
 	
 	//alterno entre las camaras y tengo una crisi porque no funciona la ortogonal :c
-	renderer.render( scene, camera );
-//}
+	renderer.render( scene, camera1 );
+}
 
-//animate();
+animate();
